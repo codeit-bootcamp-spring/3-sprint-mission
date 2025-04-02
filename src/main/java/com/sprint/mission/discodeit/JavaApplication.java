ebd1c18babbbb2c0db4581f7faf581e05a641f07
NINE-J <rnwo47@gmail.com>
2025-04-02 23:11:02 +0900

    feat: User 객체 작업, 테스트 코드 분리
    TODO: Channel, Message 객체 작업

--- src/main/java/com/sprint/mission/discodeit/JavaApplication.java
+++ src/main/java/com/sprint/mission/discodeit/JavaApplication.java
@@ -1,29 +1,7 @@
 package com.sprint.mission.discodeit;
 
-import com.sprint.mission.discodeit.entity.Common;
-
 public class JavaApplication {
 	public static void main(String[] args) {
-
-		// 여러 개의 Common 객체 테스트
-		try {
-			Common common1 = new Common();
-			System.out.println("common1 - id: " + common1.getId()
-					+ "\n common1 - createdAt: " + common1.getCreatedAt()
-					+ "\n common1 - updatedAt: " + common1.getUpdatedAt());
-			Thread.sleep(1000);
-			Common common2 = new Common();
-			System.out.println("common2 - id: " + common2.getId()
-					+ "\n common2 - createdAt: " + common2.getCreatedAt()
-					+ "\n common2 - updatedAt: " + common2.getUpdatedAt());
-			Thread.sleep(1000);
-			Common common3 = new Common();
-			System.out.println("common2 - id: " + common3.getId()
-					+ "\n common2 - createdAt: " + common3.getCreatedAt()
-					+ "\n common2 - updatedAt: " + common3.getUpdatedAt());
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
 
 		// 등록
 
