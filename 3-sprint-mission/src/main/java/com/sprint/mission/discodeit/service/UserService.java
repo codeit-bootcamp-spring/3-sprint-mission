e0cad1c32b2e6a7808a949cc1fdc62339f2b0618
pureod <sjo06102@gmail.com>
2025-04-03 11:28:56 +0900

    Implement: 서비스 인터페이스 구현

---
+++ 3-sprint-mission/src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -0,0 +1,20 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface UserService {
+
+    void create(User user);
+
+    User readById(UUID id);
+
+    List<User> readAll();
+
+    void update(UUID id, User user);
+
+    void deleteById(UUID id);
+
+}
