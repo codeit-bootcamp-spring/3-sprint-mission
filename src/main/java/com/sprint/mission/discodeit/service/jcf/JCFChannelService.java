198e15b5e882b4483882daec05c4e27772fb09ba
barrackkk <kgmlwns904@gmail.com>
2025-04-08 15:43:53 +0900

    feat(JCFChannelService): 채널 생성,조회,수정,삭제 기능 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -0,0 +1,53 @@
+package com.sprint.mission.discodeit.service.jcf;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.service.ChannelInterface;
+import java.util.List;
+
+public class JCFChannelService implements ChannelInterface {
+
+    private final List<Channel> channels;
+
+    public JCFChannelService(List<Channel> channels) {
+        this.channels = channels;
+    }
+
+    @Override
+    public void outputAllChannelInfo() {
+        for (Channel channel : channels) {
+            System.out.println(channel);
+        }
+    }
+
+    public void outputOneChannelInfo(Channel channel) {
+        if (channel == null) {
+            System.out.println("해당 채널은 존재하지 않습니다.");
+        } else {
+            System.out.println(channel);
+        }
+    }
+
+    public void updateChannelName(String oldName, String newName) {
+        channels.stream()
+                .filter(channel -> channel.getChannelName().equals(oldName))
+                .findFirst()
+                .ifPresent(channel1 -> channel1.updateChannel(newName));
+    }
+
+    public void deleteChannelName(String channelName) {
+        channels.stream()
+                .filter(channel1 -> channel1.getChannelName().equals(channelName))
+                .findFirst()
+                .ifPresent(channel1 -> channels.remove(channel1));
+    }
+
+    public void createNewChannel(String channelName) {
+        Channel channel = new Channel(channelName);
+        channels.add(channel);
+    }
+
+    public Channel changeChannel(int channelNumber) {
+        return channels.stream().filter(channel1 -> channel1.getChannelNumber() == channelNumber).findFirst()
+                .orElse(null);
+    }
+}
