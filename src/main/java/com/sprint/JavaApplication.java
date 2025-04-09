72b689ae6b0a818f292daf2bc70ed7869cb32d9a
barrackkk <kgmlwns904@gmail.com>
2025-04-09 18:12:51 +0900

    refactor: if -> switch-case으로 수정

--- src/main/java/com/sprint/JavaApplication.java
+++ src/main/java/com/sprint/JavaApplication.java
@@ -18,7 +18,7 @@ public class JavaApplication {
 
     private final static Scanner sc = new Scanner(System.in);
     private static List<User> users = new ArrayList<>();
-    
+
     public static List<Channel> channels = ChannelRepository.getChannel();
     public static List<Message> messages = new ArrayList<>();
 
@@ -35,48 +35,45 @@ public class JavaApplication {
     }
 
     private static void handleUserMenu(User user) {
-
         while (true) {
             System.out.println(
                     "1.새로운 프로필 추가\t2.모든 프로필 정보 출력\t3.프로필 정보 출력\t4.프로필 이름 수정\t5.프로필 정보 삭제\t6.프로필 변경\t7.이전 메뉴");
 
             int n = Integer.parseInt(sc.nextLine());
-            if (n == 1) {
-                System.out.println("새롭게 추가 할 프로필 이름을 입력해 주세요.");
-                String newUser = sc.nextLine();
-                jCFUserService.createNewUserNames(user.getUsername(), newUser);
-            }
-            if (n == 2) {
-                jCFUserService.outputAllUsersInfo();
-            }
-            if (n == 3) {
-                jCFUserService.outputOneUserInfo(user.getUsername());
-            }
-            if (n == 4) {
-                System.out.println("현재 사용자 이름은 " + user.getUsername() + "입니다. 새로운 이름을 입력해 주세요.");
-                String updateUserName = sc.nextLine();
-                jCFUserService.updateUserName(user.getUsername(), updateUserName);
-            }
-            if (n == 5) {
-                jCFUserService.outputAllUsersInfo();
-                System.out.println("삭제 할 사용자의 번호를 입력하세요.");
-                int num = Integer.parseInt(sc.nextLine());
-                System.out.println(user.getUsername() + " 정보를 삭제합니다.");
-                jCFUserService.deleteUserName(num);
-            }
-            if (n == 6) {
-                System.out.println("변경하실 프로필 번호를 입력하세요.");
-                jCFUserService.outputAllUsersInfo();
-                int loginNumber = Integer.parseInt(sc.nextLine());
-                login(loginNumber, users);
-                user = jCFUserService.changeUser(loginNumber);
-            }
 
-            if (n == 7) {
-                break;
+            switch (n) {
+                case 1 -> {
+                    System.out.println("새롭게 추가 할 프로필 이름을 입력해 주세요.");
+                    String newUser = sc.nextLine();
+                    jCFUserService.createNewUserNames(user.getUsername(), newUser);
+                }
+                case 2 -> jCFUserService.outputAllUsersInfo();
+                case 3 -> jCFUserService.outputOneUserInfo(user.getUsername());
+                case 4 -> {
+                    System.out.println("현재 사용자 이름은 " + user.getUsername() + "입니다. 새로운 이름을 입력해 주세요.");
+                    String updateUserName = sc.nextLine();
+                    jCFUserService.updateUserName(user.getUsername(), updateUserName);
+                }
+                case 5 -> {
+                    jCFUserService.outputAllUsersInfo();
+                    System.out.println("삭제 할 사용자의 번호를 입력하세요.");
+                    int num = Integer.parseInt(sc.nextLine());
+                    System.out.println(user.getUsername() + " 정보를 삭제합니다.");
+                    jCFUserService.deleteUserName(num);
+                }
+                case 6 -> {
+                    System.out.println("변경하실 프로필 번호를 입력하세요.");
+                    jCFUserService.outputAllUsersInfo();
+                    int loginNumber = Integer.parseInt(sc.nextLine());
+                    login(loginNumber, users);
+                    user = jCFUserService.changeUser(loginNumber);
+                }
+                case 7 -> {
+                    return;
+                }
+                default -> System.out.println("올바른 번호를 입력해 주세요.");
             }
         }
-
     }
 
     private static void handleChannelMenu(int channelNumber, List<Channel> channels) {
@@ -86,75 +83,73 @@ public class JavaApplication {
         while (true) {
             System.out.println("1.채널 생성\t2.현재 채널 정보 출력\t3.모든 채널 정보 출력\t4.채널 이름 수정\t5.채널 삭제\t6.채널 변경\t7.이전 메뉴");
             int n = Integer.parseInt(sc.nextLine());
-            if (n == 1) {
-                System.out.println("새로 추가하실 채널 이름을 입력해 주세요.");
-                String newChannelName = sc.nextLine();
-                jCFChannelService.createNewChannel(newChannelName);
-            }
-            if (n == 2) {
-                jCFChannelService.outputOneChannelInfo(selectChannel);
-            }
-            if (n == 3) {
-                jCFChannelService.outputAllChannelInfo();
-            }
-            if (n == 4) {
-                assert selectChannel != null;
-                System.out.println(
-                        "현재 접속중인 채널은" + selectChannel.getChannelName() + "입니다. 새로운 채널 이름을 작성 해 주세요.");
-                String updateChannelName = sc.nextLine();
-                jCFChannelService.updateChannelName(selectChannel.getChannelName(), updateChannelName);
-            }
-            if (n == 5) {
-                assert selectChannel != null;
-                System.out.println(selectChannel.getChannelName() + " 채널을 삭제합니다.");
-                jCFChannelService.deleteChannelName(selectChannel.getChannelName());
-                n = 7;
-            }
-            if (n == 6) {
-                System.out.println("변경하실 채널 번호를 입력해주세요.");
-                jCFChannelService.outputAllChannelInfo();
-                int newChannelNumber = Integer.parseInt(sc.nextLine());
-                selectChannel = jCFChannelService.changeChannel(newChannelNumber);
-            }
 
-            if (n == 7) {
-                break;
+            switch (n) {
+                case 1 -> {
+                    System.out.println("새로 추가하실 채널 이름을 입력해 주세요.");
+                    String newChannelName = sc.nextLine();
+                    jCFChannelService.createNewChannel(newChannelName);
+                }
+                case 2 -> jCFChannelService.outputOneChannelInfo(selectChannel);
+                case 3 -> jCFChannelService.outputAllChannelInfo();
+                case 4 -> {
+                    assert selectChannel != null;
+                    System.out.println("현재 접속중인 채널은 " + selectChannel.getChannelName() + "입니다. 새로운 채널 이름을 작성 해 주세요.");
+                    String updateChannelName = sc.nextLine();
+                    jCFChannelService.updateChannelName(selectChannel.getChannelName(), updateChannelName);
+                }
+                case 5 -> {
+                    assert selectChannel != null;
+                    System.out.println(selectChannel.getChannelName() + " 채널을 삭제합니다.");
+                    jCFChannelService.deleteChannelName(selectChannel.getChannelName());
+                    return;
+                }
+                case 6 -> {
+                    System.out.println("변경하실 채널 번호를 입력해주세요.");
+                    jCFChannelService.outputAllChannelInfo();
+                    int newChannelNumber = Integer.parseInt(sc.nextLine());
+                    selectChannel = jCFChannelService.changeChannel(newChannelNumber);
+                }
+                case 7 -> {
+                    return;
+                }
+                default -> System.out.println("올바른 번호를 입력해 주세요.");
             }
         }
     }
-
 
     private static void handleMessageMenu(User user) {
         while (true) {
             System.out.println("1.메시지 작성\t2.작성한 메시지 출력\t3.메시지 수정\t4.메시지 삭제\t5.이전 메뉴로 돌아가기");
             int n = Integer.parseInt(sc.nextLine());
-            if (n == 1) {
-                System.out.println("작성하실 메시지 입력해 주세요");
-                String messageText = sc.nextLine();
-                messages = jcfMessageService.inputMessage(messageText);
-                System.out.println("메시지 작성 완료!");
-            }
-            if (n == 2) {
-                jcfMessageService.outputUserMessage();
-            }
-            if (n == 3) {
-                System.out.println("수정 할 메시지의 번호를 입력해 주세요.");
-                jcfMessageService.outputUserMessage();
-                int messageNumber = Integer.parseInt(sc.nextLine());
-                System.out.println("메시지를 입력해 주세요.");
-                String newMessage = sc.nextLine();
-                jcfMessageService.updateUserMessage(messageNumber, newMessage);
-            }
-            if (n == 4) {
-                System.out.println("삭제한 메시지의 번호를 입력해 주세요.");
-                jcfMessageService.outputUserMessage();
-                int deleteMessageNumber = Integer.parseInt(sc.nextLine());
-                jcfMessageService.deleteUserMessage(deleteMessageNumber);
-                System.out.println("삭제가 완료됐습니다.");
-            }
 
-            if (n == 5) {
-                break;
+            switch (n) {
+                case 1 -> {
+                    System.out.println("작성하실 메시지 입력해 주세요");
+                    String messageText = sc.nextLine();
+                    messages = jcfMessageService.inputMessage(messageText);
+                    System.out.println("메시지 작성 완료!");
+                }
+                case 2 -> jcfMessageService.outputUserMessage();
+                case 3 -> {
+                    System.out.println("수정 할 메시지의 번호를 입력해 주세요.");
+                    jcfMessageService.outputUserMessage();
+                    int messageNumber = Integer.parseInt(sc.nextLine());
+                    System.out.println("메시지를 입력해 주세요.");
+                    String newMessage = sc.nextLine();
+                    jcfMessageService.updateUserMessage(messageNumber, newMessage);
+                }
+                case 4 -> {
+                    System.out.println("삭제한 메시지의 번호를 입력해 주세요.");
+                    jcfMessageService.outputUserMessage();
+                    int deleteMessageNumber = Integer.parseInt(sc.nextLine());
+                    jcfMessageService.deleteUserMessage(deleteMessageNumber);
+                    System.out.println("삭제가 완료됐습니다.");
+                }
+                case 5 -> {
+                    return;
+                }
+                default -> System.out.println("올바른 번호를 입력해 주세요.");
             }
         }
     }
