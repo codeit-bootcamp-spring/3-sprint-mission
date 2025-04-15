bd2a1bd2afe90e17e33dac49298e83b100ffeb18
DK <dongwook5478@gmail.com>
2025-04-15 15:20:04 +0900

    add: add jcf repos

---
+++ src/main/java/com/sprint/mission/discodeit/repository/jcf/JCFChannelRepository.java
@@ -0,0 +1,15 @@
+package com.sprint.mission.discodeit.repository.jcf;
+
+/**
+ * packageName    : com.sprint.mission.discodeit.repository.jcf
+ * fileName       : JCFChannelRepository
+ * author         : doungukkim
+ * date           : 2025. 4. 15.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 4. 15.        doungukkim       최초 생성
+ */
+public class JCFChannelRepository {
+}
