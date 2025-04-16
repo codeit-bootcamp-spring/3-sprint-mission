c28623823916ed752a69501840b813c5f65ec1d5
barrackkk <kgmlwns904@gmail.com>
2025-04-16 12:53:49 +0900

    feat(FileMessageService): 메시지 데이터 직렬화 기능 추가 - messages.ser에 영속화 하기 위해 loadMessages(), saveMessages() 메서드 추가

---
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
@@ -0,0 +1,84 @@
+package com.sprint.mission.discodeit.service.file;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.service.MessageService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileMessageService implements MessageService {
+
+    private List<Message> messages;
+    private final static String FILEPATH = "messages.ser";
+
+    public FileMessageService() {
+        messages = loadMessages();
+        if (messages == null) {
+            messages = new ArrayList<>();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Message> loadMessages() {
+        File file = new File(FILEPATH);
+        if (!file.exists()) {
+            return null;
+        }
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+            return (List<Message>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void saveMessages() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILEPATH))) {
+            oos.writeObject(messages);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Message> inputMessage(String newMessage) {
+        Message newMsg = new Message(newMessage);
+        messages.add(newMsg);
+        saveMessages();
+        return messages;
+    }
+
+    @Override
+    public void outputUserMessage() {
+        if (messages.isEmpty()) {
+            System.out.println("메시지가 비었습니다.");
+        } else {
+            for (Message message1 : messages) {
+                System.out.println(message1);
+            }
+        }
+    }
+
+    @Override
+    public void updateUserMessage(int number, String newMessage) {
+        messages.stream()
+                .filter(m -> m.getNumber() == number)
+                .findFirst()
+                .ifPresent(message1 -> message1.updateMessage(newMessage));
+        saveMessages();
+    }
+
+    @Override
+    public void deleteUserMessage(int number) {
+        messages.stream()
+                .filter(message1 -> message1.getNumber() == number)
+                .findFirst()
+                .ifPresent(message1 -> messages.remove(message1));
+        saveMessages();
+    }
+}
