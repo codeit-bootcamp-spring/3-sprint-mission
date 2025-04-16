8fc2d1229932e8aadba068b53e6a55dcd2118508
barrackkk <kgmlwns904@gmail.com>
2025-04-16 13:26:20 +0900

    feat(JavaApplication): 파일 기반 서비스 전환 -기존 JCF기반 서비스 구현체를 File기반 서비스 구현체로 수정

--- src/main/java/com/sprint/JavaApplication.java
+++ src/main/java/com/sprint/JavaApplication.java
@@ -1,19 +1,14 @@
 package com.sprint;
 
-import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.menu.ChannelMenu;
 import com.sprint.mission.discodeit.menu.MessageMenu;
 import com.sprint.mission.discodeit.menu.UserMenu;
-import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
-import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
-import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
-import com.sprint.mission.discodeit.service.jcf.JCFUserService;
-import java.util.ArrayList;
-import java.util.List;
+import com.sprint.mission.discodeit.service.file.FileChannelService;
+import com.sprint.mission.discodeit.service.file.FileMessageService;
+import com.sprint.mission.discodeit.service.file.FileUserService;
 import java.util.Scanner;
 
 public class JavaApplication {
@@ -21,16 +16,14 @@ public class JavaApplication {
     private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        List<User> users = new ArrayList<>();
-        List<Channel> channels = ChannelRepository.getChannel();
+        ChannelService channelService = new FileChannelService();
+        MessageService messageService = new FileMessageService();
+        UserService userService = new FileUserService();
 
-        UserService userService = new JCFUserService(users);
-        ChannelService channelService = new JCFChannelService(channels);
-        MessageService messageService = new JCFMessageService();
-
-        UserMenu userMenu = new UserMenu(sc, userService, users);
+        UserMenu userMenu = new UserMenu(sc, userService);
         ChannelMenu channelMenu = new ChannelMenu(sc, channelService);
         MessageMenu messageMenu = new MessageMenu(sc, messageService);
+
         userMenu.loginUser();
 
         System.out.println("모든 채널 정보를 출력합니다.");
@@ -45,23 +38,15 @@ public class JavaApplication {
             int choice = Integer.parseInt(sc.nextLine());
 
             switch (choice) {
-                case 1 -> {
-                    userMenu.run();
-                }
-                case 2 -> {
-                    channelMenu.run();
-                }
-                case 3 -> {
-                    messageMenu.run();
-                }
+                case 1 -> userMenu.run();
+                case 2 -> channelMenu.run();
+                case 3 -> messageMenu.run();
                 case 4 -> {
                     System.out.println("Discodeit을 종료합니다. 감사합니다.");
                     sc.close();
                     return;
                 }
-                default -> {
-                    System.out.println("올바른 번호를 입력해 주세요.");
-                }
+                default -> System.out.println("올바른 번호를 입력해 주세요.");
             }
         }
     }
