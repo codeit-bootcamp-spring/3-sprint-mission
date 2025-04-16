36cc17d189c94ab9d5fa73c6b43a1485107fbe71
barrackkk <kgmlwns904@gmail.com>
2025-04-16 11:17:58 +0900

    refactor: 변수명 통일 - scanner -> sc로 변경

--- src/main/java/com/sprint/mission/discodeit/menu/ChannelMenu.java
+++ src/main/java/com/sprint/mission/discodeit/menu/ChannelMenu.java
@@ -5,24 +5,24 @@ import com.sprint.mission.discodeit.service.ChannelService;
 import java.util.Scanner;
 
 public class ChannelMenu {
-    private final Scanner scanner;
+    private final Scanner sc;
     private final ChannelService channelService;
 
     public ChannelMenu(Scanner scanner, ChannelService channelService) {
-        this.scanner = scanner;
+        this.sc = scanner;
         this.channelService = channelService;
     }
 
     public void run() {
         while (true) {
             System.out.println("1. 채널 생성\t2. 현재 채널 정보 출력\t3. 모든 채널 정보 출력\t4. 채널 이름 수정\t5. 채널 삭제\t6. 채널 변경\t7. 이전 메뉴");
-            int n = Integer.parseInt(scanner.nextLine());
+            int n = Integer.parseInt(sc.nextLine());
             Channel currentChannel = channelService.getCurrentChannel();
 
             switch (n) {
                 case 1 -> {
                     System.out.println("새로 추가하실 채널 이름을 입력해 주세요.");
-                    String newChannelName = scanner.nextLine();
+                    String newChannelName = sc.nextLine();
                     channelService.createNewChannel(newChannelName);
                 }
                 case 2 -> channelService.outputOneChannelInfo(currentChannel);
@@ -31,7 +31,7 @@ public class ChannelMenu {
                     if (currentChannel != null) {
                         System.out.println(
                                 "현재 접속중인 채널은 " + currentChannel.getChannelName() + "입니다. 새로운 채널 이름을 입력해 주세요.");
-                        String updateChannelName = scanner.nextLine();
+                        String updateChannelName = sc.nextLine();
                         channelService.updateChannelName(currentChannel, updateChannelName);
                     }
                 }
@@ -41,14 +41,14 @@ public class ChannelMenu {
                         channelService.deleteChannelName(currentChannel);
                         System.out.println("정상적으로 채널이 삭제됐습니다. 변경하실 채널 번호를 입력해주세요.");
                         channelService.outputAllChannelInfo();
-                        int newChannelNumber = Integer.parseInt(scanner.nextLine());
+                        int newChannelNumber = Integer.parseInt(sc.nextLine());
                         channelService.changeChannel(newChannelNumber);
                     }
                 }
                 case 6 -> {
                     System.out.println("변경하실 채널 번호를 입력해주세요.");
                     channelService.outputAllChannelInfo();
-                    int newChannelNumber = Integer.parseInt(scanner.nextLine());
+                    int newChannelNumber = Integer.parseInt(sc.nextLine());
                     channelService.changeChannel(newChannelNumber);
                 }
                 case 7 -> {
