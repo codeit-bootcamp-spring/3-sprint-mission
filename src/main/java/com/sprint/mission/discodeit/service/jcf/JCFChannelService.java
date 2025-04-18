ff5d3cf5736f2be6da149c5d11c943a3f060f5cb
yeokyeong <yeokyeong1004@gmail.com>
2025-04-18 22:23:07 +0900

    refactor : redefine channel service

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFChannelService.java
@@ -1,7 +1,7 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Channel;
-import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.service.ChannelService;
 
 import java.util.*;
@@ -14,64 +14,72 @@ public class JCFChannelService implements ChannelService {
     }
 
     @Override
-    public void create(Channel ch) {
-        this.data.put(ch.getId(), ch);
+    public Channel create(String name, ChannelType type, String description) {
+        Channel channel = new Channel(name, type, description);
+        this.data.put(channel.getId(), channel);
+
+        return channel;
     }
 
     @Override
-    public Channel read(UUID id) {
-        return this.data.get(id);
+    public Channel find(UUID channelId) {
+        Channel channelNullable = this.data.get(channelId);
+
+        return Optional.ofNullable(channelNullable).orElseThrow(() -> new NoSuchElementException("Channel with id " + channelId + " not found"));
     }
 
     @Override
-    public List<Channel> readAll() {
+    public List<Channel> findAll() {
         return new ArrayList<>(this.data.values());
     }
 
     @Override
-    public Channel update(UUID id, String name) {
-        Channel selected = this.data.get(id);
-        selected.update(name);
-        return selected;
+    public Channel update(UUID channelId, String newName, String newDescription) {
+        Channel channelNullable = this.data.get(channelId);
+        Channel channel = Optional.ofNullable(channelNullable).orElseThrow(() -> new NoSuchElementException("Channel with id " + channelId + " not found"));
+        channel.update(newName, newDescription);
+
+        return channel;
     }
 
     @Override
-    public boolean delete(UUID id) {
-        this.data.remove(id);
-        //TODO : update return value
-        return true;
+    public void delete(UUID channelId) {
+        if (!this.data.containsKey(channelId)) {
+            throw new NoSuchElementException("Channel with id " + channelId + " not found");
+        }
+        this.data.remove(channelId);
     }
 
-    @Override
-    public List<User> getAttendees(Channel ch) {
-        Channel selected = this.data.get(ch.getId());
-        return selected.getAttendees();
-    }
-
-    @Override
-    public User joinChannel(Channel ch, User user) {
-        Channel selectedChannel = this.data.get(ch.getId());
-        //TODO : 참조변수 추가 방법 체크
-        selectedChannel.getAttendees().addAll(Arrays.asList(user));
-
-        return user;
-    }
-
-    @Override
-    public User leaveChannel(Channel ch, User user) {
-        Channel selectedChannel = this.data.get(ch.getId());
-        //TODO : 참조변수 방법 체크
-        selectedChannel.getAttendees().remove(user);
-
-        return user;
-    }
-
-    @Override
-    public List<User> readAttendees(Channel ch) {
-        Channel selectedChannel = this.data.get(ch.getId());
-        return selectedChannel.getAttendees();
-    }
-
+//    @Override
+//    public List<User> getAttendees(Channel ch) {
+//        Channel selected = this.data.get(ch.getId());
+//        return selected.getAttendees();
+//    }
+//
+//    @Override
+//    public User joinChannel(Channel ch, User user) {
+//        Channel selectedChannel = this.data.get(ch.getId());
+//        //TODO : 참조변수 추가 방법 체크
+//        selectedChannel.getAttendees().addAll(Arrays.asList(user));
+//
+//        return user;
+//    }
+//
+//    @Override
+//    public User leaveChannel(Channel ch, User user) {
+//        Channel selectedChannel = this.data.get(ch.getId());
+//        //TODO : 참조변수 방법 체크
+//        selectedChannel.getAttendees().remove(user);
+//
+//        return user;
+//    }
+//
+//    @Override
+//    public List<User> readAttendees(Channel ch) {
+//        Channel selectedChannel = this.data.get(ch.getId());
+//        return selectedChannel.getAttendees();
+//    }
+//
     // QUESTION :  메세지 로직은 MessageService에 있어야하는데?
 //    @Override
 //    public void sendMessage(Channel ch, Message msg) {
