621b53fc3815d4324d42c5c3f343c6e77d02f6de
barrackkk <kgmlwns904@gmail.com>
2025-04-28 13:52:11 +0900

    스프린트 미션2 재현

---
+++ src/main/java/com/sprint/mission/discodeit/entity/ReadStatus.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class ReadStatus {
+    private Instant cratedAt;
+    private Instant updatedAt;
+    
+    UUID id;
+    UUID userId;
+    UUID channelId;
+}
---
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -0,0 +1,12 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public class UserStatus {
+    private Instant cratedAt;
+    private Instant updatedAt;
+    
+    private UUID id;
+    private UUID userId;
+}
