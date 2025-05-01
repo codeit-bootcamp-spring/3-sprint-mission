70195d5115a215bd2592fb2d8071501a480d7c65
Im-Ubin <yuvin3493@gmail.com>
2025-05-01 19:29:53 +0900

    feat: AuthService 구현

--- src/main/java/com/sprint/mission/discodeit/flow/TestScenario.java
+++ src/main/java/com/sprint/mission/discodeit/flow/TestScenario.java
@@ -47,7 +47,7 @@ public class TestScenario {
 
     public void run() {
         testUserServices();
-//        testChannelServices();
+        testChannelServices();
 //        testMessageServices();
     }
 
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -9,10 +9,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class BasicAuthService implements AuthService {
     private final UserRepository userRepository;
-    private final UserStatusRepository userStatusRepository;
+    private final UserStatusRepository userStatusRepository; //이후에 login 로직에서 사용
 
     @Override
     public User authenticate(String name, String password) {
-        return null;
+        User user = userRepository.loadByName(name);
+        if (user == null) {
+            throw new IllegalArgumentException("[Auth] 유효하지 않은 사용자입니다. (" + name + ")");
+        }
+
+        if (user.getPassword().equals(password)) {
+            System.out.println(user);
+            return user;
+        } else {
+            throw new IllegalArgumentException("[Auth] 패스워드가 일치하지 않습니다. (" + name + ")");
+        }
     }
 }
