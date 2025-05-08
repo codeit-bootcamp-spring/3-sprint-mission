9b38f3dcf20de45acb872d5ac7fd3a3affe871b4
BackEunHo <bhh0214@gmail.com>
2025-05-08 17:20:22 +0900

    Spring Boot initial setup

--- src/main/java/com/sprint/mission/discodeit/run/JavaApplication.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.mission.discodeit.run;
-
-public class JavaApplication {
-
-    public static void main(String[] args) {
-        System.out.println("Sprint2 Application Started!");
-    }
-}
