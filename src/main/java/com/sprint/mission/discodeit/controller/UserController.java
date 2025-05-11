d6ecf2d7c93f4798207439c31ccdf17046407731
GeonMin02 <geonmin10@gmail.com>
2025-05-11 22:16:18 +0900

    비밀번호 검증 기능 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/UserController.java
@@ -0,0 +1,171 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserPasswordUpdateRequest;
+import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.UserStatusService;
+import com.sprint.mission.discodeit.utility.PasswordValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/* API 구현 절차
+ * 1. 엔드포인트(End-point)
+ *   - 엔드포인트는 URL과 HTTP 메서드로 구성됨.
+ *   - 엔드포인트는 다른 API와 겹치지 않는(중복되지 않는) 유일한 값으로 정의할 것.
+ * 2. 요청(Request)
+ *   - 요청으로부터 어떤 값을 받아야 하는지 정의.
+ *   - 각 값을 HTTP 요청의 Header, Body 등 어느 부분에서 어떻게 받을지 정의.
+ * 3. 응답(Response) - 뷰 기반이 아닌 데이터 기반 응답으로 작성.
+ *   - 응답 상태 코드 정의
+ *   - 응답 데이터 정의
+ *   - (옵션) 응답 헤더 정의
+ * */
+
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+@Controller
+public class UserController {
+
+    private final UserService userService;
+    private final UserStatusService userStatusService;
+
+    // 요구사항 : @RequestMapping만 사용하여 구현하기 -> method로 타입 지정( GET, POST, PUT 등 )
+    // 사용자 등록( 생성 ) : POST
+    // 사용자 정보 수정 : 전체( PUT ) / 일부( PATCH )
+    // 사용자 삭제 : DEL
+    // 전체 조회 : GET
+    // 온라인 상태 변경 : PATCH  << 전체 중 일부만 바꾸는게 확실하니
+
+    // 신규 유저 생성 요청( POST )
+    @RequestMapping(
+            // path || value : 요청 URL 경로 정의
+            // method : 요청을 처리할 HTTP 메서드 지정( GET, POST, PUT, DELETE, PATCH 등 )
+            // consumes : 데이터의 요청 MIME 타입 정의( 데이터 형식 지정 및 해당 타입만 처리하도록 제한 )
+            // produces : 컨트롤러가 반환하는 데이터의 MIME 타입 정의( JSON, XML 등 )
+            // headers : 헤더 값을 기반으로 요청 처리 지정( 특정 값 요구 혹은 제한 )
+            // params : 요청 파라미터 값 기반으로 처리 지정( 요청 | 쿼리 파라미터를 특정 값으로 제한 )
+            path = "/create"
+            , method = RequestMethod.POST
+            , consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    @ResponseBody
+    // ? : WildCard( 어느 타입이든 처리 ) << User로 지정 시 에러 메세지 출력 불가의 이유로 변경
+    public ResponseEntity<?> create(
+            // @RequestPart : 하나 일 때는 value 생략 가능, 다수일 경우 이후부터 value 필요
+            // + value의 값이 선택적이라면 required를 false로 지정( 필수 : true( 기본 ) / 선택적 : false )
+            @RequestPart("userCreateRequest") UserCreateRequest userCreateRequest,
+            @RequestPart(value = "profile", required = false) MultipartFile profile
+    ) {
+
+        // 보안성 향상을 위한 비밀번호 검증 로직
+        List<String> errors = PasswordValidator.getPasswordValidationErrors(userCreateRequest.getPwd());
+
+        // 비밀번호 규칙 검증시 false 시
+        if (!errors.isEmpty()) {
+            // 규칙 검증 시 발생한 에러문 응답
+            String errorMessage = String.join("\n", errors);
+            // 응답 상태( 상태 코드 : 400 ) 및 내부 정보( 비밀번호 검증 시 발생한 에러 메세지 ) 반환
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
+        // 선택적 유저 프로필 이미지 처리
+        Optional<BinaryContentCreateRequest> profileRequest =
+                Optional.ofNullable(profile)
+                        // 선택적 프로필 이미지의 값 여부 판별 메서드 동작 후 해당 값을 저장
+                        .flatMap(this::resolveProfileRequest);
+
+        User createdUser = userService.create(userCreateRequest, profileRequest);
+
+        // HTTP 응답 커스터마이징
+        // 상태코드 : 생성됨( 201 )
+        // 응답 상태( 상태 코드 : 201 ), 내부 정보( 유저 생성 DTO, 선택적 프로필 이미지 ) 반환
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    // MultipartFile 타입의 요청값을 BinaryContentCreateRequest 타입으로 변환하기 위한 메서드
+    // MultipartFile : 파일 업로드를 처리할 때 사용하는 인터페이스
+    private Optional<BinaryContentCreateRequest> resolveProfileRequest(MultipartFile profile) {
+
+        if(profile.isEmpty()) {
+            // 컨트롤러가 요청받은 파라미터 중 MultipartFile 타입의 데이터가 비어있다면:
+            return Optional.empty();
+        } else {
+            // 컨트롤러가 요청받은 파라미터 중 MultipartFile 타입의 데이터가 존재한다면:
+            try {
+                BinaryContentCreateRequest binaryContentCreateRequest = new BinaryContentCreateRequest(
+                        profile.getOriginalFilename(),
+                        profile.getBytes(),
+                        profile.getContentType()
+                );
+                return Optional.of(binaryContentCreateRequest);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+
+    @RequestMapping(
+            path = "/{userId}"
+            // 전체적인 정보 수정이라 예상, PUT 선택
+            , method = RequestMethod.PUT
+            , consumes = { MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE }
+    )
+    @ResponseBody
+    // 사용자 정보 수정( PUT / PATCH )
+    public ResponseEntity<User> update(
+            // 어느 사용자인지 식별
+            @PathVariable UUID userId
+            , @RequestPart("userUpdateRequest") UserUpdateRequest userUpdateRequest
+            , @RequestPart(value = "profile", required = false) MultipartFile profile
+            ) {
+        User updatedUser = userService.update(userId, userUpdateRequest, Optional.ofNullable(profile).flatMap(this::resolveProfileRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedUser);
+    }
+
+
+
+
+
+    // 비밀번호 변경 전용
+    @RequestMapping(
+            path = "/{userId}/password"
+            , method = RequestMethod.PATCH
+    )
+    @ResponseBody
+    // 에러 메세지( String )를 출력하고자 제네릭을 와일드카드 타입으로 사용
+    public ResponseEntity<?> updateByPass(
+            @PathVariable UUID userId
+            , @RequestPart("userPasswordUpdateRequest") UserPasswordUpdateRequest userPasswordUpdateRequest
+    ) {
+
+        // 새 비밀번호 규칙 검증( 대소문자, 숫자, 특수문자 포함 + 8자 이상 )
+        List<String> errors = PasswordValidator.getPasswordValidationErrors(userPasswordUpdateRequest.getNewPassword());
+
+        // 비밀번호 규칙 검증시 false 시
+        if (!errors.isEmpty()) {
+            // 규칙 검증 시 발생한 에러문 응답
+            String errorMessage = String.join("\n", errors);
+            // 응답 상태( 상태 코드 : 400 ) 및 내부 정보( 비밀번호 검증 시 발생한 에러 메세지 ) 반환
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+
+        // 비밀번호 규칙 검증이 ture 시, 비밀번호 변경
+        User updatedUser = userService.updateByPass(userId, userPasswordUpdateRequest);
+
+        return ResponseEntity.ok(updatedUser);
+    }
+}
