36795fa13a601d1e2f7e27e49245ebf95b744470
BackEunHo <bhh0214@gmail.com>
2025-05-12 18:12:59 +0900

    feat: Add BinaryContentService interface and its basic implementation for managing binary content

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicBinaryContentService.java
@@ -0,0 +1,56 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.repository.BinaryContentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BasicBinaryContentService implements BinaryContentService {
+
+    private final BinaryContentRepository binaryContentRepository;
+
+    @Transactional
+    @Override
+    public BinaryContent create(BinaryContentCreateRequest request) {
+        BinaryContent newBinaryContent = new BinaryContent(
+                request.fileName(),
+                (long) request.bytes().length,
+                request.contentType(),
+                request.bytes()
+        );
+
+        return binaryContentRepository.save(newBinaryContent);
+    }
+
+    @Override
+    public BinaryContent find(UUID binaryContentId) {
+        return binaryContentRepository.findById(binaryContentId)
+                .orElseThrow(() -> new NoSuchElementException("BinaryContent not found with id: " + binaryContentId));
+    }
+
+    @Override
+    public List<BinaryContent> findAllByIdIn(List<UUID> binaryContentIds) {
+        return binaryContentRepository.findAllByIdIn(binaryContentIds);
+    }
+
+    @Transactional
+    @Override
+    public void delete(UUID binaryContentId) {
+        if (!binaryContentRepository.existsById(binaryContentId)) {
+            throw new NoSuchElementException("BinaryContent not found with id: " + binaryContentId + ". Cannot delete.");
+        }
+
+        binaryContentRepository.deleteById(binaryContentId);
+    }
+}
