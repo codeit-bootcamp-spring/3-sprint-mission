f677078fa8a06ec4571514d6d0fd8d6a655581bf
Kangmoongu <sprtms5325@naver.com>
2025-05-15 19:32:35 +0900

    fix: 피드백반영(orElse(), orElseGet() 사용)

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -22,94 +22,106 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class BasicUserService implements UserService {
 
-    private final UserRepository userRepository;
-    private final UserStatusRepository userStatusRepository;
-    private final BinaryContentRepository binaryContentRepository;
+  private final UserRepository userRepository;
+  private final UserStatusRepository userStatusRepository;
+  private final BinaryContentRepository binaryContentRepository;
 
 
-    @Override
-    public User create(CreateUserRequest userRequest, Optional<CreateBinaryContentRequest> binaryContentRequest) {
-        User user = new User(null,userRequest.username(), userRequest.password(), userRequest.email());
-        //중복검사후 아닐경우 save
-        try{
-            if(!userRepository.duplicateCheck(user)) {
-                if (binaryContentRequest.isPresent()) {
-                    BinaryContent binaryContent = new BinaryContent(binaryContentRequest.get().filename(),binaryContentRequest.get().contentType(), binaryContentRequest.get().bytes());
-                    binaryContentRepository.save(binaryContent);
-                    user.setProfileId(binaryContent.getId());
-                }
-                userRepository.save(user);
-                userStatusRepository.save(new UserStatus(user.getId()));
-                return user;
-            }else
-                throw new DuplicateRequestException();
-        }catch (DuplicateRequestException e) {
-            System.out.println("중복된 name 또는 email 입니다.");
-        }
-        return null;
+  @Override
+  public User create(CreateUserRequest userRequest,
+      Optional<CreateBinaryContentRequest> binaryContentRequest) {
+    User user = new User(null, userRequest.username(), userRequest.password(), userRequest.email());
+    //중복검사후 아닐경우 save
+    try {
+      if (!userRepository.duplicateCheck(user)) {
+        binaryContentRequest.ifPresent(request -> {
+          BinaryContent binaryContent = new BinaryContent(
+              request.filename(),
+              request.contentType(),
+              request.bytes()
+          );
+          binaryContentRepository.save(binaryContent);
+          user.setProfileId(binaryContent.getId());
+        });
+        userRepository.save(user);
+        userStatusRepository.save(new UserStatus(user.getId()));
+        return user;
+      } else {
+        throw new DuplicateRequestException();
+      }
+    } catch (DuplicateRequestException e) {
+      System.out.println("중복된 name 또는 email 입니다.");
+    }
+    return null;
+  }
+
+  @Override
+  public List<UserDto> findAll() {
+    List<User> userList = userRepository.read();
+    List<UserDto> findUserRespondList = new ArrayList<>();
+    findUserRespondList = userList.stream()
+        .map(user -> new UserDto(user.getId(), user.getCreatedAt(), user.getUpdatedAt(),
+            user.getUsername(), user.getEmail(), user.getProfileId(),
+            userStatusRepository.readByUserId(user.getId()).get().IsOnline()))
+        .collect(Collectors.toList());
+    return findUserRespondList;
+  }
+
+  @Override
+  public UserDto find(UUID id) {
+    Optional<User> user = userRepository.readById(id);
+    try {
+      return user.map(
+              u -> new UserDto(u.getId(), u.getCreatedAt(), u.getUpdatedAt(), u.getUsername(),
+                  u.getEmail(), u.getProfileId(),
+                  userStatusRepository.readByUserId(u.getId()).get().getOnline()))
+          .orElseThrow(NoSuchElementException::new);
+    } catch (NoSuchElementException e) {
+      System.out.println(e);
+      return null;
     }
 
-    @Override
-    public List<UserDto> findAll() {
-        List<User> userList = userRepository.read();
-        List<UserDto> findUserRespondList = new ArrayList<>();
-        findUserRespondList = userList.stream()
-                .map(user->new UserDto(user.getId(),user.getCreatedAt(),user.getUpdatedAt(),user.getUsername(),user.getEmail(),user.getProfileId(),userStatusRepository.readByUserId(user.getId()).get().IsOnline()))
-                .collect(Collectors.toList());
-        return findUserRespondList;
+  }
+
+  @Override
+  public void update(UpdateUserRequest request,
+      Optional<CreateBinaryContentRequest> binaryContentRequest) {
+    User user = new User(null, request.username(), request.password(), request.email());
+    user.setFriends(request.friends());
+    UUID profileId = userRepository.readById(request.id()).get().getProfileId();
+
+    binaryContentRequest.ifPresent(
+        b -> {
+          BinaryContent binaryContent = new BinaryContent(b.filename(), b.contentType(), b.bytes());
+          if (profileId != null) {
+            binaryContentRepository.update(profileId, binaryContent);
+          } else {
+            binaryContentRepository.save(binaryContent);
+            user.setProfileId(binaryContent.getId());
+          }
+        });
+    userRepository.update(request.id(), user);
+
+  }
+
+  @Override
+  public void delete(UUID userId) {
+    Optional<User> user = userRepository.readById(userId);
+    UUID profileId;
+    try {
+      if (user.isPresent()) {
+        User user1 = user.get();
+        profileId = user1.getProfileId();
+        UserStatus userStatus = userStatusRepository.readByUserId(user1.getId()).get();
+        userStatusRepository.delete(userStatus.getId());
+        binaryContentRepository.delete(profileId);
+        userRepository.delete(userId);
+      } else {
+        throw new NoSuchElementException("존재하지 않는 userId입니다.");
+      }
+    } catch (NoSuchElementException e) {
+      System.out.println(e);
     }
 
-    @Override
-    public UserDto find(UUID id) {
-        Optional<User> user = userRepository.readById(id);
-        try {
-            if (user.isPresent()) {
-                UserDto findUserRespond = new UserDto(user.get().getId(),user.get().getCreatedAt(),user.get().getUpdatedAt(),user.get().getUsername(),user.get().getEmail(),user.get().getProfileId(),userStatusRepository.readByUserId(user.get().getId()).get().IsOnline());
-                return findUserRespond;
-            } else {
-                throw new NoSuchElementException("존재하지 않는 id 입니다.");
-            }
-        }catch (NoSuchElementException e){
-            System.out.println(e);
-            return null;
-        }
-
-    }
-
-    @Override
-    public void update(UpdateUserRequest request,Optional<CreateBinaryContentRequest> binaryContentRequest) {
-        User user = new User(null, request.username(), request.password(), request.email());
-        user.setFriends(request.friends());
-        if(binaryContentRequest.isPresent()) {
-            BinaryContent binaryContent = new BinaryContent(binaryContentRequest.get().filename(), binaryContentRequest.get().contentType(), binaryContentRequest.get().bytes());
-            if (binaryContentRepository.readById(userRepository.readById(request.id()).get().getProfileId()).isEmpty()) {
-                binaryContentRepository.save(binaryContent);
-            } else {
-                binaryContentRepository.update(request.id(), binaryContent);
-            }
-        }
-        userRepository.update(request.id(), user);
-    }
-
-    @Override
-    public void delete(UUID userId) {
-        Optional<User> user = userRepository.readById(userId);
-        UUID profileId;
-        try {
-            if (user.isPresent()) {
-                User user1 = user.get();
-                profileId = user1.getProfileId();
-                UserStatus userStatus = userStatusRepository.readByUserId(user1.getId()).get();
-                userStatusRepository.delete(userStatus.getId());
-                binaryContentRepository.delete(profileId);
-                userRepository.delete(userId);
-            } else {
-                throw new NoSuchElementException("존재하지 않는 userId입니다.");
-            }
-        }
-        catch (NoSuchElementException e){
-            System.out.println(e);
-        }
-
-    }
+  }
 }
