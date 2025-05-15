a0a03dcfdf2eb513f212447f70544a7720d09120
Dw-real <gksehddn8@naver.com>
2025-05-15 17:23:33 +0900

    ExceptionHandler에 정의되지 않은 예외 처리 기능 추가

--- src/main/java/com/sprint/mission/discodeit/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/handler/GlobalExceptionHandler.java
@@ -15,31 +15,43 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(LoginFailedException.class)
-    public ResponseEntity<String> handleLoginFailedException(Exception e) {
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED) // 401
-                .body(e.getMessage());
-    }
+  @ExceptionHandler(LoginFailedException.class)
+  public ResponseEntity<String> handleLoginFailedException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.UNAUTHORIZED) // 401
+        .body(e.getMessage());
+  }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(Exception e) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(e.getMessage());
-    }
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<String> handleNotFoundException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.NOT_FOUND)
+        .body(e.getMessage());
+  }
 
-    @ExceptionHandler({DuplicateException.class, AlreadyExistsException.class})
-    public ResponseEntity<String> handleConflictException(Exception e) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(e.getMessage());
-    }
+  @ExceptionHandler({DuplicateException.class, AlreadyExistsException.class})
+  public ResponseEntity<String> handleConflictException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.CONFLICT)
+        .body(e.getMessage());
+  }
 
-    @ExceptionHandler({NotFriendsException.class, PrivateChannelModificationException.class, UserNotInChannelException.class})
-    public ResponseEntity<String> handleBadRequestException(Exception e) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
-    }
+  @ExceptionHandler({NotFriendsException.class, PrivateChannelModificationException.class,
+      UserNotInChannelException.class})
+  public ResponseEntity<String> handleBadRequestException(Exception e) {
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(e.getMessage());
+  }
+
+  /* 정의되지 않은 예외 처리 */
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<String> handleUnhandledException(Exception e) {
+
+    e.printStackTrace();
+
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body("서버 내부 오류가 발생했습니다.");
+  }
 }
