31d58a677081567c9ba36c53ee1f298347f8f7ad
okod <okodee@naver.com>
2025-05-16 10:11:46 +0900

    feat: 바이너리 파일 다운로드 Controller 구현

---
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -0,0 +1,38 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/binaryContent")
+@Controller
+public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    /**
+     * 바이너리 파일 단건 조회
+     */
+    @RequestMapping(path = "/find/{binaryContentId}")
+    public ResponseEntity<BinaryContent> find(@PathVariable("binaryContentId") UUID binaryContentId) {
+        BinaryContent binaryContent = binaryContentService.find(binaryContentId);
+        return ResponseEntity.ok(binaryContent);
+    }
+
+    /**
+     * 바이너리 파일 다건 조회
+     */
+    @RequestMapping(path = "/findAll/{binaryContentIds}")
+    public ResponseEntity<List<BinaryContent>> findAllByIdIn(@PathVariable("binaryContentIds") List<UUID> binaryContentIds) {
+        List<BinaryContent> binaryContents = binaryContentService.findAllByIdIn(binaryContentIds);
+        return ResponseEntity.ok(binaryContents);
+    }
+}
