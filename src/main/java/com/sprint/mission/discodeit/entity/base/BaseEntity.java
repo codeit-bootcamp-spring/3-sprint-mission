528b98dc4bd617fadeef49038cdd8b6c71fe216a
Kangmoongu <sprtms5325@naver.com>
2025-05-27 13:17:26 +0900

    feat: JPA의 어노테이션(@CreatedDate, @LastModifiedDate)을 활용하여 속성 자동설정

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -2,9 +2,12 @@ package com.sprint.mission.discodeit.entity.base;
 
 import java.time.Instant;
 import java.util.UUID;
+import org.springframework.data.annotation.CreatedDate;
 
 public abstract class BaseEntity {
 
   UUID id;
+
+  @CreatedDate
   Instant createdAt;
 }
--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.discodeit.entity.base;
 
 import java.time.Instant;
+import org.springframework.data.annotation.LastModifiedDate;
 
 public class BaseUpdatableEntity extends BaseEntity {
 
+  @LastModifiedDate
   Instant updatedAt;
 }
