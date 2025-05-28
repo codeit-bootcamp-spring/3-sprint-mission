690f638bca7dd71f3b8d8841de6fff8e5eb7bc04
Kangmoongu <sprtms5325@naver.com>
2025-05-28 16:20:24 +0900

    feat: modelmapper 의존성 삭제, Mapper 추가, Dto 추가

---
+++ src/main/java/com/sprint/mission/discodeit/mapper/ChannelMapper.java
@@ -0,0 +1,43 @@
+package com.sprint.mission.discodeit.mapper;
+
+import com.sprint.mission.discodeit.dto.data.ChannelDto;
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.ReadStatusRepository;
+import java.util.Comparator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ChannelMapper {
+
+  private final MessageRepository messageRepository;
+  private final ReadStatusRepository readStatusRepository;
+  private final UserMapper userMapper;
+
+  public ChannelDto toDto(Channel channel) {
+    return new ChannelDto(
+        channel.getId(),
+        channel.getType(),
+        channel.getName(),
+        channel.getDescription(),
+        readStatusRepository
+            .findAllByChannelId(channel.getId())
+            .stream()
+            .map(ReadStatus::getUser)
+            .map(userMapper::toDto)
+            .toList(),
+        messageRepository
+            .findAllByChannelId(channel.getId())
+            .stream()
+            .map(Message::getCreatedAt)
+            .max(Comparator.naturalOrder())
+            .orElse(null)
+    );
+
+
+  }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/mapper/ReadStatusMapper.java
@@ -0,0 +1,17 @@
+package com.sprint.mission.discodeit.mapper;
+
+import com.sprint.mission.discodeit.dto.data.ReadStatusDto;
+import com.sprint.mission.discodeit.entity.ReadStatus;
+
+public class ReadStatusMapper {
+
+  public ReadStatusDto toDto(ReadStatus readStatus) {
+    return new ReadStatusDto(
+        readStatus.getId(),
+        readStatus.getUser().getId(),
+        readStatus.getChannel().getId(),
+        readStatus.getLastReadAt()
+    );
+  }
+
+}
