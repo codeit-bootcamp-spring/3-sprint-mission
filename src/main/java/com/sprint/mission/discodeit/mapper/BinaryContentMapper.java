bd609d7e933e5232e929fdf83792cd9bdb4b8f04
Kangmoongu <sprtms5325@naver.com>
2025-06-03 09:30:15 +0900

    feat: MapStruct 의존성 추가 및 BinaryContentMapper에 적용

--- src/main/java/com/sprint/mission/discodeit/mapper/BinaryContentMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/BinaryContentMapper.java
@@ -1,27 +1,14 @@
 package com.sprint.mission.discodeit.mapper;
 
+
 import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.BinaryContent;
-import com.sprint.mission.discodeit.storage.BinaryContentStorage;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Component
-@RequiredArgsConstructor
-public class BinaryContentMapper {
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface BinaryContentMapper {
 
-  public BinaryContentDto toDto(BinaryContent binaryContent) {
-
-    if (binaryContent != null) {
-      return new BinaryContentDto(
-          binaryContent.getId(),
-          binaryContent.getFileName(),
-          binaryContent.getSize(),
-          binaryContent.getContentType(),
-          null);
-    } else {
-      return null;
-    }
-  }
+  public BinaryContentDto toDto(BinaryContent binaryContent);
 
 }
