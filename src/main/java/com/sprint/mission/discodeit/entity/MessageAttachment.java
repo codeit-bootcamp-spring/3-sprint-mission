8162e776fb1c0b93bd7e59a9fb36c61d61cacd0a
BackEunHo <bhh0214@gmail.com>
2025-06-03 11:09:51 +0900

    feat(entity): Add composite key support to the MessageAttachment entity and refactor it

--- src/main/java/com/sprint/mission/discodeit/entity/MessageAttachment.java
+++ src/main/java/com/sprint/mission/discodeit/entity/MessageAttachment.java
@@ -1,8 +1,13 @@
 package com.sprint.mission.discodeit.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
 
 @Entity
 @Table(name = "message_attachments")
@@ -10,18 +15,36 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MessageAttachment {
 
-  @Id
+  @EmbeddedId
+  private MessageAttachmentId id;
+
   @ManyToOne
+  @MapsId("messageId")
   @JoinColumn(name = "message_id", nullable = false)
   private Message message;
 
-  @Id
   @ManyToOne
+  @MapsId("attachmentId")
   @JoinColumn(name = "attachment_id", nullable = false)
   private BinaryContent attachment;
 
   public MessageAttachment(Message message, BinaryContent attachment) {
     this.message = message;
     this.attachment = attachment;
+    this.id = new MessageAttachmentId(message.getId(), attachment.getId());
+  }
+
+  @Embeddable
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @EqualsAndHashCode
+  @Getter
+  public static class MessageAttachmentId implements Serializable {
+
+    @Column(name = "message_id")
+    private UUID messageId;
+
+    @Column(name = "attachment_id")
+    private UUID attachmentId;
   }
 }
