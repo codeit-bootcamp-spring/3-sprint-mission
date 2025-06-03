1d47b20d13d22e69e3b3ec1ed318e4e8d77c80a8
okod <okodee@naver.com>
2025-06-03 13:16:23 +0900

    feat: Implement JPA Auditing for createdAt and updatedAt

--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseEntity.java
@@ -2,9 +2,11 @@ package com.sprint.mission.discodeit.entity.base;
 import java.time.Instant;
 import java.util.UUID;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
 
 @Getter
 public abstract class BaseEntity {
     private UUID id;
+    @CreatedDate
     private Instant createdAt;
 }
--- src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
+++ src/main/java/com/sprint/mission/discodeit/entity/base/BaseUpdatableEntity.java
@@ -2,8 +2,10 @@ package com.sprint.mission.discodeit.entity.base;
 
 import java.time.Instant;
 import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Getter
 public abstract class BaseUpdatableEntity extends BaseEntity{
+    @LastModifiedDate
     protected Instant updatedAt;
 }
