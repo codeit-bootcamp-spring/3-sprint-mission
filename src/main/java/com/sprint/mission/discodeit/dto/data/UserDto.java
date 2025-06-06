9ce4379a229b1c9dbbf00ee60a2db485926e999f
okod <okodee@naver.com>
2025-06-06 21:52:30 +0900

    feat: data DTO 정의

--- src/main/java/com/sprint/mission/discodeit/dto/data/UserDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/data/UserDto.java
@@ -1,16 +1,13 @@
 package com.sprint.mission.discodeit.dto.data;
 
-import com.sprint.mission.discodeit.entity.User;
 import java.time.Instant;
 import java.util.UUID;
 
 public record UserDto(
         UUID id,
-        Instant createdAt,
-        Instant updatedAt,
         String username,
         String email,
-        UUID profileId,
+        BinaryContentDto profile,
         Boolean online
 ) {
 }
