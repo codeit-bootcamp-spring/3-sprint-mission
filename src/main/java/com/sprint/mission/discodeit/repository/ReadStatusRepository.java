400d66903d8789d22a72800a105fde2341121dd3
Im-Ubin <yuvin3493@gmail.com>
2025-06-08 17:21:18 +0900

    feat: N+1 문제 관련 코드 수정, OSIV 비활성화

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -4,11 +4,14 @@ import com.sprint.mission.discodeit.entity.ReadStatus;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReadStatusRepository extends JpaRepository<ReadStatus, UUID> {
 
+  @Query("SELECT r FROM ReadStatus r JOIN FETCH r.user")
   List<ReadStatus> findAllByUserId(UUID userId);
 
+  @Query("SELECT r FROM ReadStatus r JOIN FETCH r.channel")
   List<ReadStatus> findAllByChannelId(UUID channelId);
 
   void deleteAllByChannelId(UUID channelId);
