af53360390bbdda57c4d4af61577d174a83ff4cb
Im-Ubin <yuvin3493@gmail.com>
2025-06-08 17:21:18 +0900

    feat: N+1 문제 관련 코드 수정, OSIV 비활성화

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -5,9 +5,11 @@ import java.util.UUID;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
 
+  @Query("SELECT m FROM Message m JOIN FETCH m.channel")
   Slice<Message> findAllByChannelId(UUID channelId, Pageable pageable);
 
   void deleteAllByChannelId(UUID channelId);
--- src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/UserRepository.java
@@ -2,11 +2,18 @@ package com.sprint.mission.discodeit.repository;
 
 import com.sprint.mission.discodeit.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+  @Query("SELECT u FROM User u JOIN FETCH u.status")
+  @NonNull
+  List<User> findAll();
 
   Optional<User> findByUsername(String username);
 
