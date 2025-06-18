13a5d92d03212fe3e2a04753209fe5fb2a897630
Dw-real <gksehddn8@naver.com>
2025-06-18 17:45:58 +0900

    feat: 개발, 운영 환경에 대한 프로파일 구성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,20 +1,17 @@
 -- 1. distcodeit 스키마 생성
 CREATE SCHEMA IF NOT EXISTS discodeit;
 -- 2. 권한 설정
-GRANT ALL PRIVILEGES ON SCHEMA discodeit TO discodeit_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA discodeit TO discodeit_user;
+--GRANT ALL PRIVILEGES ON SCHEMA discodeit TO discodeit_user;
+--GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA discodeit TO discodeit_user;
 -- 3. 권한 설정 확인
-SELECT
-    n.nspname AS schema_name,
-    pg_get_userbyid(n.nspowner) AS schema_owner
-FROM
-    pg_namespace n
-WHERE
-    n.nspname = 'discodeit';
+-- SELECT n.nspname                   AS schema_name,
+--        pg_get_userbyid(n.nspowner) AS schema_owner
+-- FROM pg_namespace n
+-- WHERE n.nspname = 'discodeit';
 -- 4. 검색 경로 설정
-ALTER ROLE discodeit_user SET search_path TO discodeit, public;
+-- ALTER ROLE discodeit_user SET search_path TO discodeit, public;
 -- 5. 검색 경로 확인
-SHOW search_path;
+-- SHOW search_path;
 -- 6. 테이블 삭제
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS binary_contents CASCADE;
@@ -26,22 +23,22 @@ DROP TABLE IF EXISTS messages CASCADE;
 -- 7. 테이블 생성
 CREATE TABLE IF NOT EXISTS binary_contents
 (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL,
-    file_name VARCHAR(255) NOT NULL,
-    size BIGINT NOT NULL,
-    content_type VARCHAR(100) NOT NULL,
+    id           UUID,
+    created_at   TIMESTAMP with time zone NOT NULL,
+    file_name    VARCHAR(255)             NOT NULL,
+    size         BIGINT                   NOT NULL,
+    content_type VARCHAR(100)             NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(60) NOT NULL,
+    id         UUID,
+    created_at TIMESTAMP with time zone NOT NULL,
+    updated_at TIMESTAMP with time zone,
+    username   VARCHAR(50)              NOT NULL UNIQUE,
+    email      VARCHAR(100)             NOT NULL UNIQUE,
+    password   VARCHAR(60)              NOT NULL,
     profile_id UUID,
     PRIMARY KEY (id),
     FOREIGN KEY (profile_id)
@@ -51,11 +48,11 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS user_statuses
 (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
-    user_id UUID NOT NULL UNIQUE,
-    last_active_at TIMESTAMPTZ NOT NULL,
+    id             UUID,
+    created_at     TIMESTAMP with time zone NOT NULL,
+    updated_at     TIMESTAMP with time zone,
+    user_id        UUID                     NOT NULL UNIQUE,
+    last_active_at TIMESTAMP with time zone NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id)
         REFERENCES users (id)
@@ -64,24 +61,24 @@ CREATE TABLE IF NOT EXISTS user_statuses
 
 CREATE TABLE IF NOT EXISTS channels
 (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
-    name VARCHAR(100),
+    id          UUID,
+    created_at  TIMESTAMP with time zone NOT NULL,
+    updated_at  TIMESTAMP with time zone,
+    name        VARCHAR(100),
     description VARCHAR(500),
-    type VARCHAR(20) NOT NULL,
+    type        VARCHAR(20)              NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT channel_type_check CHECK (type IN ('PUBLIC', 'PRIVATE'))
 );
 
 CREATE TABLE IF NOT EXISTS messages
 (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
-    content TEXT,
+    id         UUID,
+    created_at TIMESTAMP with time zone NOT NULL,
+    updated_at TIMESTAMP with time zone,
+    content    TEXT,
     channel_id UUID,
-    author_id UUID,
+    author_id  UUID,
     PRIMARY KEY (id),
     FOREIGN KEY (channel_id)
         REFERENCES channels (id)
@@ -93,12 +90,12 @@ CREATE TABLE IF NOT EXISTS messages
 
 CREATE TABLE IF NOT EXISTS read_statuses
 (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
-    user_id UUID,
-    channel_id UUID,
-    last_read_at TIMESTAMPTZ NOT NULL,
+    id           UUID,
+    created_at   TIMESTAMP with time zone NOT NULL,
+    updated_at   TIMESTAMP with time zone,
+    user_id      UUID,
+    channel_id   UUID,
+    last_read_at TIMESTAMP with time zone NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id)
         REFERENCES users (id)
@@ -111,7 +108,7 @@ CREATE TABLE IF NOT EXISTS read_statuses
 
 CREATE TABLE IF NOT EXISTS message_attachments
 (
-    message_id UUID,
+    message_id    UUID,
     attachment_id UUID,
     PRIMARY KEY (message_id, attachment_id),
     FOREIGN KEY (message_id)
