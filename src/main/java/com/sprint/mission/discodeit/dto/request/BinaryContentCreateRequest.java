a36e3abfa9872765d18d81a03a62f0fe8a1dceb3
barrackkk <kgmlwns904@gmail.com>
2025-06-19 15:25:34 +0900

    feat: 제약 조건 관련 어노테이션 추가

--- src/main/java/com/sprint/mission/discodeit/dto/request/BinaryContentCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/BinaryContentCreateRequest.java
@@ -1,9 +1,12 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record BinaryContentCreateRequest(
-    String fileName,
-    String contentType,
-    byte[] bytes
+    @NotBlank String fileName,
+    @NotBlank String contentType,
+    @NotNull byte[] bytes
 ) {
 
 }
--- src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelCreateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/PublicChannelCreateRequest.java
@@ -1,6 +1,11 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record PublicChannelCreateRequest(
+    @NotBlank
+    @Size(min = 1, max = 20, message = "채널 이름은 1자 이상, 20자 이하여야 합니다.")
     String name,
     String description
 ) {
