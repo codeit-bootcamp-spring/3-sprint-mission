030e357ea5c65fd27e2428050ed79128f2ec1500
GeonMin02 <geonmin10@gmail.com>
2025-06-19 11:10:08 +0900

    refactor: H2 호환성을 위해 DDL문의 timestamptz 타입을 timestamp with time zone으로 변경

--- src/main/resources/shema.sql
+++ src/main/resources/shema.sql
@@ -5,11 +5,11 @@ CREATE TABLE binary_contents
     id           uuid PRIMARY KEY,
 
     -- Column
-    created_at   timestamptz  NOT NULL,
-    file_name    varchar(255) NOT NULL,
-    size         bigint       NOT NULL,
-    content_type varchar(100) NOT NULL,
-    bytes        bytea        NOT NULL
+    created_at   timestamp with time zone NOT NULL,
+    file_name    varchar(255)             NOT NULL,
+    size         bigint                   NOT NULL,
+    content_type varchar(100)             NOT NULL,
+    bytes        bytea                    NOT NULL
 );
 
 
@@ -20,11 +20,11 @@ CREATE TABLE users
     id         uuid PRIMARY KEY,
 
     -- Column
-    created_at timestamptz  NOT NULL,
-    updated_at timestamptz,
-    username   varchar(50)  NOT NULL,
-    email      varchar(100) NOT NULL,
-    password   varchar(60)  NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    username   varchar(50)              NOT NULL,
+    email      varchar(100)             NOT NULL,
+    password   varchar(60)              NOT NULL,
     profile_id uuid,
 
     -- Unique Key( username, email )
@@ -46,10 +46,10 @@ CREATE TABLE user_statuses
     id             uuid PRIMARY KEY,
 
     -- Column
-    created_at     timestamptz NOT NULL,
-    updated_at     timestamptz,
-    user_id        uuid        NOT NULL,
-    last_active_at timestamptz NOT NULL,
+    created_at     timestamp with time zone NOT NULL,
+    updated_at     timestamp with time zone,
+    user_id        uuid                     NOT NULL,
+    last_active_at timestamp with time zone NOT NULL,
 
     -- Unique Key ( user_id )
     CONSTRAINT uq_user_id UNIQUE (user_id),
@@ -69,11 +69,11 @@ CREATE TABLE channels
     id          uuid PRIMARY KEY,
 
     -- Column
-    created_at  timestamptz NOT NULL,
-    updated_at  timestamptz,
+    created_at  timestamp with time zone NOT NULL,
+    updated_at  timestamp with time zone,
     name        varchar(100),
     description varchar(500),
-    type        varchar(10) NOT NULL,
+    type        varchar(10)              NOT NULL,
 
     -- ENUM ( type )
     -- CHECK () : 칼럼 저장 값 조건 명시, 해당 조건 만족 시에만 값 저장 제한
@@ -88,11 +88,11 @@ CREATE TABLE read_statuses
     id           uuid PRIMARY KEY,
 
     -- Column
-    created_at   timestamptz NOT NULL,
-    updated_at   timestamptz,
+    created_at   timestamp with time zone NOT NULL,
+    updated_at   timestamp with time zone,
     user_id      uuid,
     channel_id   uuid,
-    last_read_at timestamptz NOT NULL,
+    last_read_at timestamp with time zone NOT NULL,
 
     -- Unique Key( user_id, channel_id )
     CONSTRAINT uq_read_user_channel UNIQUE (user_id, channel_id),
@@ -118,10 +118,10 @@ CREATE TABLE messages
     id         uuid PRIMARY KEY,
 
     -- Column
-    created_at timestamptz NOT NULL,
-    updated_at timestamptz,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
     content    text,
-    channel_id uuid        NOT NULL,
+    channel_id uuid                     NOT NULL,
     author_id  uuid,
 
     -- Foreign Key( channel_id, author_id )
