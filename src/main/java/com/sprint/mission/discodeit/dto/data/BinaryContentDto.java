3a7d11636642f8b247aa2afac5f97d8d3d3b1231
yeokyeong <yeokyeong1004@gmail.com>
2025-06-20 16:08:40 +0900

    chore : update README

---
+++ src/main/java/com/sprint/mission/discodeit/dto/data/BinaryContentDto.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.dto.data;
+
+import java.util.UUID;
+/* byte[] bytes 가 엔티티에는 없고 dto는 있어서 값을 넣어주려면 불변 객체인 record는 사용 불가 */
+
+public record BinaryContentDto(
+    UUID id,
+    String fileName,
+    Long size,
+    String contentType
+) {
+
+}
