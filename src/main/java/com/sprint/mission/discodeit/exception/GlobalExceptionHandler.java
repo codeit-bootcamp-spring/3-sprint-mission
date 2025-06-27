ae29cba79167bb408834d6087e1282264c526aeb
Im-Ubin <yuvin3493@gmail.com>
2025-06-27 21:22:58 +0900

    feat: 글로벌 예외 처리 코드 수정, ErrorResponseMapper 클래스 구현

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -1,50 +1,24 @@
 package com.sprint.mission.discodeit.exception;
 
-import com.sprint.mission.discodeit.dto.response.ErrorCode;
-import org.springframework.http.HttpStatus;
+import com.sprint.mission.discodeit.dto.response.ErrorResponse;
+import com.sprint.mission.discodeit.mapper.ErrorResponseMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.NoSuchElementException;
-
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+  private final ErrorResponseMapper mapper;
 
   @ExceptionHandler(DiscodeitException.class)
   public ResponseEntity<ErrorResponse> handleDiscodeitException(DiscodeitException e) {
-    ErrorCode errorCode = e.getErrorCode();          // enum 에 담긴 코드/메시지
-    body = new ErrorResponse(
-        e.getTimestamp(),
-        errorCode.getCode(),
-        errorCode.getMessage(),
-        e.getDetails()
-    );
+    ErrorResponse errorResponse = mapper.toErrorResponse(e);
 
     return ResponseEntity
-        .status(errorCode.getStatus())
-        .body(body);
-  }
-
-  @ExceptionHandler(IllegalArgumentException.class)
-  public ResponseEntity<String> handleException(IllegalArgumentException e) {
-    return ResponseEntity
-        .status(HttpStatus.BAD_REQUEST)
-        .body(e.getMessage());
-  }
-
-  @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<String> handleException(NoSuchElementException e) {
-    return ResponseEntity
-        .status(HttpStatus.NOT_FOUND)
-        .body(e.getMessage());
-  }
-
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<String> handleException(Exception e) {
-    return ResponseEntity
-        .status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(e.getMessage());
+        .status(e.getErrorCode().getStatus())
+        .body(errorResponse);
   }
 }
