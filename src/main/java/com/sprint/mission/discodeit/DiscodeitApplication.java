734cdb54c01014d786470e605e47b2d227b508a9
adjoon1 <minj97@naver.com>
2025-06-30 04:23:24 +0900

    feat: 예외 처리 및 로깅 설정 완료

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,16 +2,10 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {
-    "com.sprint.mission.discodeit.controller",
-    "com.sprint.mission.discodeit.service",
-    "com.sprint.mission.discodeit.repository.jcf",
-    "com.sprint.mission.discodeit.repository.file"
-})
 public class DiscodeitApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DiscodeitApplication.class, args);
     }
---
+++ src/main/java/com/sprint/mission/discodeit/config/JpaAuditingConfig.java
@@ -0,0 +1,5 @@
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {}
