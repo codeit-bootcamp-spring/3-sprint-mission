94f9550f3180fc68e36178f3db2dd694d5210ac7
barrackkk <kgmlwns904@gmail.com>
2025-07-03 16:15:00 +0900

    fix: entrypoint 제거 및 실행 명령어 수정

--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "► JVM_OPTS: $JVM_OPTS"
-echo "► 실행 대상 JAR: build/libs/${PROJECT_NAME}-${PROJECT_VERSION}.jar"
-
-exec java $JVM_OPTS -jar build/libs/${PROJECT_NAME}-${PROJECT_VERSION}.jar
