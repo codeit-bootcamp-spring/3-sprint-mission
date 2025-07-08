f7227d7e63265e98a8dd3dee097289ad431cf6bd
GeonMin02 <geonmin10@gmail.com>
2025-07-08 15:34:59 +0900

    refactor: BinaryContentMapper의 고정 타겟 매핑 제거

--- src/main/java/com/sprint/mission/discodeit/mapper/BinaryContentMapper.java
+++ src/main/java/com/sprint/mission/discodeit/mapper/BinaryContentMapper.java
@@ -3,12 +3,10 @@ package com.sprint.mission.discodeit.mapper;
 import com.sprint.mission.discodeit.dto.data.BinaryContentDto;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BinaryContentMapper {
 
-    @Mapping(target = "bytes", expression = "java(null)")
     BinaryContentDto toDto(BinaryContent binaryContent);
 
 }
