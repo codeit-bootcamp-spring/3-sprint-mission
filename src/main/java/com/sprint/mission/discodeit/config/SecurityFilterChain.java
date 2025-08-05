d552a951efadb82dd8bf825e8b44221c269777b3
barrackkk <kgmlwns904@gmail.com>
2025-08-05 23:32:13 +0900

    feat: 로그아웃을 처리할 url을  /api/auth/logout로 설정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityFilterChain.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityFilterChain.java
@@ -5,11 +5,13 @@ import com.sprint.mission.discodeit.security.LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
@@ -28,6 +30,12 @@ public class SecurityFilterChain {
                 .loginProcessingUrl("/api/auth/login")
                 .successHandler(loginSuccessHandler)
                 .failureHandler(loginFailureHandler)
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .logoutUrl("/api/auth/logout")
+                .logoutSuccessHandler(
+                    new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT)) // 204
                 .permitAll()
             )
             .csrf(csrf -> csrf
