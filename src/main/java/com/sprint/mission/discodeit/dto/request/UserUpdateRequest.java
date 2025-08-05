d4302305db35889dcbf688b1ba1a4ad1000cca8e
barrackkk <kgmlwns904@gmail.com>
2025-08-05 17:09:23 +0900

    feat: 유효성 검사 @NotBlank 제거

--- src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/UserUpdateRequest.java
@@ -1,15 +1,14 @@
 package com.sprint.mission.discodeit.dto.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserUpdateRequest(
-    @NotBlank @Size(min = 3, max = 20)
+    @Size(min = 3, max = 20)
     String newUsername,
-    @NotBlank @Email
+    @Email
     String newEmail,
-    @NotBlank @Size(min = 6, max = 30)
+    @Size(min = 6, max = 30)
     String newPassword
 ) {
 
