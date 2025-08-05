649b11d47043de36a7e9aa6d6a4c091c7e88c83c
Kangmoongu <sprtms5325@naver.com>
2025-08-05 11:23:50 +0900

    feat: sprint9 베이스코드 반영

---
+++ discodeit/src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package com.sprint.mission.discodeit.config;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public CommandLineRunner debugFilterChain(SecurityFilterChain filterChain) {
+        return args -> {
+            int filterSize = filterChain.getFilters().size();
+
+            List<String> filterNames = IntStream.range(0, filterSize)
+                    .mapToObj(idx -> String.format("\t[%s/%s] %s", idx + 1, filterSize,
+                            filterChain.getFilters().get(idx).getClass()))
+                    .toList();
+
+            System.out.println("현재 적용된 필터 체인 목록:");
+            filterNames.forEach(System.out::println);
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return
+                http
+                        .csrf(csrf -> csrf
+                                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                                .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
+
+                        .formLogin(login -> login
+                                .loginProcessingUrl("/api/auth/login"))
+
+                        .build();
+    }
+}
