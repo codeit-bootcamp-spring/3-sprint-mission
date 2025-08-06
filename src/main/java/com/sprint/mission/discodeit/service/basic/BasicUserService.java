72359d3685053b13fe7338ac27495939ef2f197f
barrackkk <kgmlwns904@gmail.com>
2025-08-06 10:44:44 +0900

    feat: Service 메서드 별로 권한 수정
    - 퍼블릭 채널 생성, 수정, 삭제는 CHANNEL_MANAGER
    - 사용자 권한 수정은 ADMIN

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -22,6 +22,7 @@ import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -163,6 +164,7 @@ public class BasicUserService implements UserService {
     }
 
     @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
     public UserDto updateRole(UUID userId, Role newRole) {
         User user = userRepository.findById(userId)
             .orElseThrow(() -> new UserNotFoundException(userId));
