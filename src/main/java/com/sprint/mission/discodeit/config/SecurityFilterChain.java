cfde32c87ade8656c07b1e6ad96eb4cf9fccf0af
barrackkk <kgmlwns904@gmail.com>
2025-08-06 11:06:55 +0900

    feat: 권한의 계층 구조 정의

--- src/main/java/com/sprint/mission/discodeit/config/SecurityFilterChain.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityFilterChain.java
@@ -12,6 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -105,4 +109,19 @@ public class SecurityFilterChain {
         };
     }
 
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+
+        RoleHierarchy hierarchy = RoleHierarchyImpl.fromHierarchy(
+            "ROLE_ADMIN > ROLE_CHANNEL_MANAGER > ROLE_USER");
+        return hierarchy;
+    }
+
+    @Bean
+    static MethodSecurityExpressionHandler methodSecurityExpressionHandler(
+        RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        return handler;
+    }
 }
