dde17c58bf423228279ea2ee115322f496165251
bs8841 <whqortjsgkgk@naver.com>
2025-08-07 13:08:47 +0900

    feat: UserDetails 구현체 추가

--- src/main/java/com/sprint/mission/discodeit/auth/service/DiscodeitUserDetailsService.java
+++ src/main/java/com/sprint/mission/discodeit/auth/service/DiscodeitUserDetailsService.java
@@ -19,8 +19,8 @@ public class DiscodeitUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        return userRepository.findByUsername(username)
-            .map(DiscodeitUserDetails::new)
-            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+        return new DiscodeitUserDetails(userMapper.toDto(user), user.getPassword());
     }
 }
