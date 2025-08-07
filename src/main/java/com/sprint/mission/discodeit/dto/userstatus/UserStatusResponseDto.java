b37ff030a138745c46cdc64e3880466516e7535a
Dw-real <gksehddn8@naver.com>
2025-08-07 18:45:49 +0900

    chore: UserStatus 관련 DTO 삭제

--- src/main/java/com/sprint/mission/discodeit/dto/userstatus/UserStatusResponseDto.java
+++
@@ -1,6 +0,0 @@
-package com.sprint.mission.discodeit.dto.userstatus;
-
-import java.time.Instant;
-import java.util.UUID;
-
-public record UserStatusResponseDto(UUID id, UUID userId, Instant lastActiveAt) {}
