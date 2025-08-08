a3487c77d9d840b63140e9c3585669d006a4320d
bs8841 <whqortjsgkgk@naver.com>
2025-08-08 15:24:12 +0900

    feat: UserDetails 권한 매핑 및 UserDto.role 추가

--- src/main/java/com/sprint/mission/discodeit/auth/service/DiscodeitUserDetails.java
+++ src/main/java/com/sprint/mission/discodeit/auth/service/DiscodeitUserDetails.java
@@ -6,6 +6,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
@@ -17,7 +18,8 @@ public class DiscodeitUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        String authority = "ROLE_" + userDto.role().name();
+        return List.of(new SimpleGrantedAuthority(authority));
     }
 
     @Override
