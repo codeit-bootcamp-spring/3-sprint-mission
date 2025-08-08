085e06ed02d8436deafecb4763476c6640ff193b
bs8841 <whqortjsgkgk@naver.com>
2025-08-08 14:25:05 +0900

    feat: 회원가입 시 기본 권한 USER 부여

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicUserService.java
@@ -5,6 +5,7 @@ import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import com.sprint.mission.discodeit.exception.user.UserAlreadyExistsException;
@@ -68,6 +69,7 @@ public class BasicUserService implements UserService {
     String encodedPassword = passwordEncoder.encode(userCreateRequest.password());
 
     User user = new User(username, email, encodedPassword, nullableProfile);
+    user.updateRole(Role.USER);
     Instant now = Instant.now();
     UserStatus userStatus = new UserStatus(user, now);
 
