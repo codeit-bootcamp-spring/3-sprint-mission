3eaffc78caef059be8d3d68a3af89f0e87c6929c
okodee <okodee@naver.com>
2025-08-10 17:53:35 +0900

    refactor: 기존 로그인 관련 코드 제거

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,18 +1,12 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
-import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
-import com.sprint.mission.discodeit.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,21 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RestController
 public class AuthController implements AuthApi {
-
-    private final AuthService authService;
-
-    @PostMapping(path = "/login")
-    @Override
-    public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
-        log.info("로그인 요청: username={}", loginRequest.username());
-
-        UserDto user = authService.login(loginRequest);
-
-        log.debug("로그인 응답: {}", user);
-
-        return ResponseEntity.ok(user);
-    }
-
     /**
      * CSRF 토큰 발급 API
      * <p>
