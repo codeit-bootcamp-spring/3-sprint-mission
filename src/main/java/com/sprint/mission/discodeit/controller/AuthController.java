687caff3461c052e4b353102b63c0b41c69570be
okodee <okodee@naver.com>
2025-08-11 14:41:47 +0900

    feat: 사용자 권한 수정 API 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
 import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.UserService;
 import java.util.UUID;
@@ -12,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,6 +48,13 @@ public class AuthController implements AuthApi {
         log.info("내 정보 조회 요청");
         UUID userId = userDetails.getUserDto().id();
         UserDto userDto = userService.find(userId);
+        return ResponseEntity.ok(userDto);
+    }
+
+    @PutMapping("/role")
+    public ResponseEntity<UserDto> role(@RequestBody RoleUpdateRequest request) {
+        log.info("권한 수정 요청");
+        UserDto userDto = userService.updateRole(request);
         return ResponseEntity.ok(userDto);
     }
 }
--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -2,6 +2,7 @@ package com.sprint.mission.discodeit.service;
 
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
+import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
 import java.util.List;
@@ -14,4 +15,5 @@ public interface UserService {
     List<UserDto> findAll();
     UserDto update(UUID userId, UserUpdateRequest userUpdateRequest, Optional<BinaryContentCreateRequest> profileCreateRequest);
     void delete(UUID userId);
+    UserDto updateRole(RoleUpdateRequest request);
 }
