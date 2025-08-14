1390cbde033b942b09e099647852899762b02c2f
Leichtstar <leichtnidel@gmail.com>
2025-08-14 15:59:15 +0900

    fix: 불필요한 /api/auth/login 엔드포인트에 대한 권한부여 삭제

--- src/main/java/com/sprint/mission/discodeit/security/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/security/config/SecurityConfig.java
@@ -73,7 +73,6 @@ public class SecurityConfig {
 				// 회원가입 모두허용
 				.requestMatchers(HttpMethod.POST, "/api/users").permitAll()
 				// 로그인 모두허용
-				.requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
 				.requestMatchers(HttpMethod.GET, "/api/auth/me").permitAll()
 				// 로그아웃 모두허용
 				.requestMatchers(HttpMethod.POST, "/api/auth/logout").permitAll()
