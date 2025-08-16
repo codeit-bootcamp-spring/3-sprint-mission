fdd40ad4eeb16623587166887be34fb6deef5868
okodee <okodee@naver.com>
2025-08-16 16:20:57 +0900

    refactor: exceptionHandling refactoring

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.sprint.mission.discodeit.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sprint.mission.discodeit.entity.Role;
-import com.sprint.mission.discodeit.security.CustomAccessDeniedHandler;
+import com.sprint.mission.discodeit.security.Http403ForbiddenAccessDeniedHandler;
 import com.sprint.mission.discodeit.security.LoginFailureHandler;
 import com.sprint.mission.discodeit.security.LoginSuccessHandler;
+import java.util.List;
+import java.util.stream.IntStream;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,6 +20,8 @@ import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,40 +29,27 @@ import org.springframework.security.web.authentication.Http403ForbiddenEntryPoin
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-
-    // 사용자의 비밀번호를 BCrypt 암호화하기 위한 Bean 설정
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public SecurityFilterChain filterChain(
         HttpSecurity http,
         LoginSuccessHandler loginSuccessHandler,
         LoginFailureHandler loginFailureHandler,
-        CustomAccessDeniedHandler customAccessDeniedHandler) throws Exception {
+        ObjectMapper objectMapper,
+        SessionRegistry sessionRegistry) throws Exception {
         http
             .csrf(csrf -> csrf
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler())
-            )
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(
-                    AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/api/auth/csrf-token"),
-                    AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/users"),
-                    AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/login"),
-                    AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/logout"),
-                    new NegatedRequestMatcher(AntPathRequestMatcher.antMatcher("/api/**"))
-                ).permitAll()
-                .anyRequest().authenticated()
             )
             // Form 기반 로그인 활성화
             .formLogin(login -> login
@@ -67,12 +61,39 @@ public class SecurityConfig {
                 .logoutUrl("/api/auth/logout")
                 .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.NO_CONTENT))
             )
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                    AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/api/auth/csrf-token"),
+                    AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/users"),
+                    AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/login"),
+                    AntPathRequestMatcher.antMatcher(HttpMethod.POST, "/api/auth/logout"),
+                    new NegatedRequestMatcher(AntPathRequestMatcher.antMatcher("/api/**"))
+                ).permitAll()
+                .anyRequest().authenticated()
+            )
             .exceptionHandling(ex -> ex
                 .authenticationEntryPoint(new Http403ForbiddenEntryPoint())
-                .accessDeniedHandler(customAccessDeniedHandler)
+                .accessDeniedHandler(new Http403ForbiddenAccessDeniedHandler(objectMapper))
             );
 
         return http.build();
+    }
+
+    @Bean
+    public CommandLineRunner debugFilterChain(SecurityFilterChain filterChain) {
+        return args -> {
+            int filterSize = filterChain.getFilters().size();
+            List<String> filterNames = IntStream.range(0, filterSize)
+                .mapToObj(idx -> String.format("\t[%s/%s] %s", idx + 1, filterSize,
+                    filterChain.getFilters().get(idx).getClass()))
+                .toList();
+            log.debug("Debug Filter Chain...\n{}", String.join(System.lineSeparator(), filterNames));
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -93,5 +114,15 @@ public class SecurityConfig {
         DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
         handler.setRoleHierarchy(roleHierarchy);
         return handler;
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
     }
 }
--- src/main/java/com/sprint/mission/discodeit/security/Http403ForbiddenAccessDeniedHandler.java
+++ src/main/java/com/sprint/mission/discodeit/security/Http403ForbiddenAccessDeniedHandler.java
@@ -1,40 +1,30 @@
 package com.sprint.mission.discodeit.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sprint.mission.discodeit.exception.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-public class CustomAccessDeniedHandler implements AccessDeniedHandler  {
+@RequiredArgsConstructor
+public class Http403ForbiddenAccessDeniedHandler implements AccessDeniedHandler  {
 
     private final ObjectMapper objectMapper;
-
-    public CustomAccessDeniedHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
 
     @Override
     public void handle(HttpServletRequest request,
         HttpServletResponse response,
         AccessDeniedException accessDeniedException) throws IOException, ServletException {
-
-        // JSON 에러 응답 생성
-        Map<String, Object> errorResponse = new HashMap<>();
-        errorResponse.put("success", false);
-        errorResponse.put("error", "ACCESS_DENIED");
-        errorResponse.put("message", "해당 리소스에 접근할 권한이 없습니다.");
-        errorResponse.put("status", 403);
-
-        // 응답 헤더 설정
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
 
+        ErrorResponse errorResponse = new ErrorResponse(accessDeniedException,
+            HttpServletResponse.SC_FORBIDDEN);
         response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
