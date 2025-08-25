46b0abae43e68c7583ec6f70bfdffdf290b702ad
barrackkk <kgmlwns904@gmail.com>
2025-08-25 13:09:24 +0900

    feat: 이벤트를 받아 실제 바이너리 데이터를 저장하는 리스너를 구현

---
+++ src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
@@ -0,0 +1,34 @@
+// src/main/java/com/sprint/mission/discodeit/event/BinaryContentEventListener.java
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.storage.BinaryContentStorage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class BinaryContentEventListener {
+
+    private final BinaryContentStorage storage;
+    private final BinaryContentService binaryContentService;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handle(BinaryContentCreatedEvent event) {
+        try {
+            storage.put(event.binaryContentId(), event.bytes(), event.contentType());
+
+            binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.SUCCESS);
+            log.info("[BinaryContent] 저장 성공: id={}", event.binaryContentId());
+        } catch (Exception e) {
+
+            binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.FAIL);
+            log.error("[BinaryContent] 저장 실패: id={}", event.binaryContentId(), e);
+        }
+    }
+}
