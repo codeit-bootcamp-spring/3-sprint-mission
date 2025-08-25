71043f6df640eb9a6fa28f4577f95ed07f9aa13f
barrackkk <kgmlwns904@gmail.com>
2025-08-25 16:14:24 +0900

    feat: 알림 관련 엔티티, 서비스 등 필요한 기능 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicNotificationService.java
@@ -0,0 +1,59 @@
+package com.sprint.mission.discodeit.service.basic;
+
+import com.sprint.mission.discodeit.dto.data.NotificationDto;
+import com.sprint.mission.discodeit.entity.Notification;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.exception.notification.NotificationAccessDeniedException;
+import com.sprint.mission.discodeit.exception.notification.NotificationNotFoundException;
+import com.sprint.mission.discodeit.mapper.NotificationMapper;
+import com.sprint.mission.discodeit.repository.NotificationRepository;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.NotificationService;
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class BasicNotificationService implements NotificationService {
+
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final NotificationMapper notificationMapper;
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public List<NotificationDto> findAllByReceiverId(UUID userId) {
+        return notificationRepository.findAllByReceiverIdAndConfirmedFalse(userId).stream()
+                .map(notificationMapper::toDto)
+                .toList();
+    }
+
+
+    @Override
+    @Transactional
+    public void confirm(UUID notificationId, UUID userId) {
+        Notification notification = notificationRepository.findById(notificationId)
+                .orElseThrow(() -> new NotificationNotFoundException(notificationId));
+
+        if (!notification.getReceiver().getId().equals(userId)) {
+            throw new NotificationAccessDeniedException(notificationId);
+        }
+
+        notification.confirm();
+    }
+
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void send(User receiver, String title, String content) {
+        Notification notification = new Notification(receiver, title, content);
+        notificationRepository.save(notification);
+        log.info("알림 저장 완료 → to={}, title={}", receiver.getUsername(), title);
+    }
+}
