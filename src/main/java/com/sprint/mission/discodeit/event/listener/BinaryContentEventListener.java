49d700df354977cc631d6ee2ff10806d3cbe610e
Kangmoongu <sprtms5325@naver.com>
2025-08-25 17:51:14 +0900

    feat: 비동기 실패 처리하기 구현

--- src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
+++ src/main/java/com/sprint/mission/discodeit/event/listener/BinaryContentEventListener.java
@@ -2,11 +2,19 @@ package com.sprint.mission.discodeit.event.listener;
 
 
 import com.sprint.mission.discodeit.entity.BinaryContentStatus;
+import com.sprint.mission.discodeit.entity.Notification;
+import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.event.BinaryContentCreatedEvent;
+import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.service.BinaryContentService;
+import com.sprint.mission.discodeit.service.NotificationService;
 import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jboss.logging.MDC;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -19,9 +27,14 @@ public class BinaryContentEventListener {
 
     private final BinaryContentStorage storage;
     private final BinaryContentService binaryContentService;
-
+    private final NotificationService notificationService;
+    private final UserRepository userRepository;
 
     @Async("asyncExecutor")
+    @Retryable(
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onBinaryContentCreated(BinaryContentCreatedEvent event) {
         log.info("바이너리 컨텐츠 스토리지 저장 로직 시작 id: {}", event.binaryContentId());
@@ -33,5 +46,21 @@ public class BinaryContentEventListener {
         }
         binaryContentService.updateStatus(event.binaryContentId(), BinaryContentStatus.SUCCESS);
         log.info("바이너리 컨텐츠 저장완료!");
+    }
+
+    @Recover
+    public void recover(Exception e, BinaryContentCreatedEvent event) {
+
+        String requestId = (String) MDC.get("requestId");
+        String title = "S3 파일 업로드 실패";
+
+        String content = String.format("""
+                Request ID: %s
+                BinaryContent ID: %s
+                Error: %s
+                """, requestId, event.binaryContentId(), e.getMessage());
+
+        User admin = userRepository.findByUsername("admin").get();
+        notificationService.create(admin,title,content);
     }
 }
