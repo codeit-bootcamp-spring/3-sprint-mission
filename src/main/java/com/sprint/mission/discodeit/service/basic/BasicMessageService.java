3ec768f3abace9f3926811e09c4735db4cf7d7b1
barrackkk <kgmlwns904@gmail.com>
2025-08-25 13:10:53 +0900

    refactor: 바이너리 데이터 저장 로직 변경에 따른 로직 리펙토링

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicMessageService.java
@@ -9,6 +9,7 @@ import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.event.BinaryContentCreatedEvent;
 import com.sprint.mission.discodeit.exception.channel.ChannelNotFoundException;
 import com.sprint.mission.discodeit.exception.message.AuthorNotFoundException;
 import com.sprint.mission.discodeit.exception.message.MessageNotFoundException;
@@ -20,13 +21,13 @@ import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.MessageService;
-import com.sprint.mission.discodeit.storage.BinaryContentStorage;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.security.access.AccessDeniedException;
@@ -45,9 +46,9 @@ public class BasicMessageService implements MessageService {
     private final ChannelRepository channelRepository;
     private final UserRepository userRepository;
     private final MessageMapper messageMapper;
-    private final BinaryContentStorage binaryContentStorage;
     private final BinaryContentRepository binaryContentRepository;
     private final PageResponseMapper pageResponseMapper;
+    private final ApplicationEventPublisher publisher;
 
     @Transactional
     @Override
@@ -85,9 +86,11 @@ public class BasicMessageService implements MessageService {
                             contentType);
                     binaryContentRepository.save(binaryContent);
 
-                    binaryContentStorage.put(binaryContent.getId(), bytes, contentType);
-
-                    log.debug("첨부파일 저장 완료: id={}, fileName={}", binaryContent.getId(), fileName);
+                    // 이벤트 발행
+                    publisher.publishEvent(new BinaryContentCreatedEvent(
+                            binaryContent.getId(), contentType, bytes
+                    ));
+                    log.debug("첨부 메타 저장 완료(이벤트 발행): id={}, fileName={}", binaryContent.getId(), fileName);
                     return binaryContent;
                 })
                 .toList();
