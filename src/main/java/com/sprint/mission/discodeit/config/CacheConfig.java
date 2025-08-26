09fca9388ec61dd03c5828673bec6d6d6986ece1
Kangmoongu <sprtms5325@naver.com>
2025-08-26 18:47:16 +0900

    feat: Redis Cache 도입하기 구현

--- src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/CacheConfig.java
@@ -1,5 +1,8 @@
 package com.sprint.mission.discodeit.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.CacheManager;
@@ -9,6 +12,9 @@ import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 import java.util.List;
@@ -123,6 +129,26 @@ public class CacheConfig {
         );
         manager.setCacheNames(List.of("userById","users"));
         return manager;
+    }
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration(ObjectMapper objectMapper) {
+        ObjectMapper redisObjectMapper = objectMapper.copy();
+        redisObjectMapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.EVERYTHING,
+                JsonTypeInfo.As.PROPERTY
+        );
+
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer(redisObjectMapper)
+                        )
+                )
+                .prefixCacheNameWith("discodeit:")
+                .entryTtl(Duration.ofSeconds(600))
+                .disableCachingNullValues();
     }
 
 
