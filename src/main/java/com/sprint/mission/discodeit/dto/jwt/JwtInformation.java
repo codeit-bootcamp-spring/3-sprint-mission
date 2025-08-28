7e2c6c1b7822d9f42a82a4fd38497460230b80a1
Dw-real <gksehddn8@naver.com>
2025-08-28 11:51:30 +0900

    chore: JwtDto, JwtInformation 주석 추가

--- src/main/java/com/sprint/mission/discodeit/dto/jwt/JwtInformation.java
+++ src/main/java/com/sprint/mission/discodeit/dto/jwt/JwtInformation.java
@@ -2,6 +2,14 @@ package com.sprint.mission.discodeit.dto.jwt;
 
 import java.util.UUID;
 
+/**
+ * 서버에서 JWT를 관리하기 위한 내부 저장 객체
+ *
+ * @param userId       사용자 ID
+ * @param username     토큰과 연결된 계정명
+ * @param accessToken  인증 요청 시 사용할 액세스 토큰
+ * @param refreshToken AccessToken 재발급 시 사용하는 토큰
+ */
 public record JwtInformation(
     UUID userId,
     String username,
