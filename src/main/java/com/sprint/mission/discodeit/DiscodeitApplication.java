c1b81c0d93c11f01127bc6a98ede8c36154ddd29
NINE <99791512+NINE-J@users.noreply.github.com>
2025-08-30 16:54:57 +0900

    chore: Redis 도입 전역 캐시 저장소 구성
    - Caffeine 인메모리 -> Redis
    - DataGrip > Redis 접속 후 영속화된 데이터 확인

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -2,10 +2,8 @@ package com.sprint.mission.discodeit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class DiscodeitApplication {
 
   public static void main(String[] args) {
