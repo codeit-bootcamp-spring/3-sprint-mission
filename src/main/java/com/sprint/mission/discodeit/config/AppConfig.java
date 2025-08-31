ede7938b3953210997ae542a4834b890b63c323c
Im-Ubin <yuvin3493@gmail.com>
2025-08-31 14:19:23 +0900

    chore: Spring Retry 환경 구성

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -4,11 +4,13 @@ import com.sprint.mission.discodeit.storage.s3.AwsProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableJpaAuditing
 @EnableScheduling
+@EnableRetry
 @EnableConfigurationProperties(AwsProperties.class)
 public class AppConfig {
 
