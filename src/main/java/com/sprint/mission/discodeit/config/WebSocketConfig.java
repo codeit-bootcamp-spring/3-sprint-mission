dba44441133a827f61d48afb7a18b3b0a6af782d
Kangmoongu <sprtms5325@naver.com>
2025-09-08 15:05:22 +0900

    feat: 웹소켓 인증/인가 처리하기

--- src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/WebSocketConfig.java
@@ -1,14 +1,23 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.entity.Role;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.security.messaging.context.SecurityContextChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final JwtAuthenticationChannelInterceptor jwtAuthenticationChannelInterceptor;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -23,5 +32,19 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             .withSockJS()
             .setHeartbeatTime(25000)       // 25초마다 하트비트 전송
             .setDisconnectDelay(5000);   // 5초 후 연결 해제
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtAuthenticationChannelInterceptor,
+            new SecurityContextChannelInterceptor());
+    }
+
+    private AuthorizationChannelInterceptor authorizationChannelInterceptor() {
+        return new AuthorizationChannelInterceptor(
+            MessageMatcherDelegatingAuthorizationManager.builder()
+                .anyMessage().hasRole(Role.USER.name())
+                .build()
+        );
     }
 }
